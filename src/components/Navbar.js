9af0d49d14eb91c126ef5f42579af25cb2a63684
kimjinhyuk <kimjinhyuk2@gmail.com>
2020-10-20 06:35:56 +0900

    update navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+
+
+function Navbar() {
+  return (
+    <div>
+      <>
+        <nav className="navbar">
+          <div className="navber-container">
+            
+          </div>
+        </nav>
+      </>    
+    </div>
+  )
+}
+
+export default Navbar
