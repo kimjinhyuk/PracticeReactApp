13b95bf63a849fedc4c2ba5139e8d25b598590d7
kimjinhyuk <kimjinhyuk2@gmail.com>
2020-10-21 05:05:59 +0900

    Change Home page with video

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className="hero-container">
-      {/* <video src="/videos/video-2.mp4" autoPlay loop muted /> */}
+      <video src="/videos/video-1.mp4" autoPlay loop muted />
       <h1>Febreath</h1>
       <p>영상/음성 데이터로 코로나19 진단</p>
       <div className="hero-btns">
