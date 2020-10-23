5d7cb6b4b1586655eb7e2bda5dc269e658238824
kimjinhyuk <kimjinhyuk2@gmail.com>
2020-10-24 05:18:38 +0900

    add axios package

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
-import Home from './components/pages/Home'
-import About from './components/pages/About'
-import Record from './components/pages/Record'
-import SignUp from './components/pages/SignUp'
-import Models from './components/pages/Models'
-import Screening from './components/pages/Screening'
-import Test from './components/pages/Test'
-import Visualization from './components/pages/Visualization'
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Record from './components/pages/Record';
+import SignUp from './components/pages/SignUp';
+import Models from './components/pages/Models';
+import Screening from './components/pages/Screening';
+import Test from './components/pages/Test';
+import Visualization from './components/pages/Visualization';
 
 
 function App() {
--- src/components/pages/Test.js
+++ src/components/pages/Test.js
@@ -1,6 +1,20 @@
-import React from 'react'
-import '../../App.css'
+import React from 'react';
+import '../../App.css';
 
 export default function Test(){
   return <h1 className='test'>Test</h1>
 }
+
+const axios = require('axios');
+
+async function makeRequest() {
+
+    const config = {
+        method: 'get',
+        url: 'http://localhost:8000/predict/image/'
+    }
+    let res = await axios(config)
+    console.log(res.data);
+}
+
+makeRequest();
